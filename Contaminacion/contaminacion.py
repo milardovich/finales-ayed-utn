26378e7d401c209eb046f8eaec61176c9200f9c5
Sergio Milardovich <sergio@polluxcoop.com>
2022-09-23 17:14:19 -0300

    WIP contaminacion

--- Contaminacion/contaminacion.py
+++ Contaminacion/contaminacion.py
@@ -1,8 +1,9 @@
 import os
 import pickle
-import os.path
 import datetime
 from random import randint
+
+medicionesDiarias = [[0 for x in range(2)] for y in range(15)]
 
 class Medicion:
     def __init__(self):
@@ -17,16 +18,65 @@ def formatearMedicion(medicion):
 
 def inicializarMediciones():
     global alMediciones
-    for i in range(15):
+    alMediciones.seek(0)
+    for i in range(100):
         aux = Medicion()
-        aux.nro_zona = i
-        aux.snap = randint(0, 500)
+        aux.nro_zona = randint(0, 14)
+        aux.snap = randint(0, 350)
+        formatearMedicion(aux)
         pickle.dump(aux, alMediciones)
-        
+        alMediciones.flush()
+
+def calidadAire(snap):
+    if (snap <= 50):
+        return 'Buena'
+    elif (snap <= 100):
+        return 'Moderada'
+    elif (snap <= 150):
+        return 'Dañina para grupos sensibles'
+    elif (snap <= 200):
+        return 'Dañina para la salud'
+    elif (snap <= 300):
+        return 'Muy dañina para la salud'
+    else:
+        return 'Peligrosa'
+
 def informeDiario():
-    global alMediciones, afMediciones
-    alMediciones.seek(0)        
+    global afMediciones, alMediciones, medicionesDiarias
+
+    t = os.path.getsize(afMediciones)
+    alMediciones.seek(0)
+
+    while alMediciones.tell() < t:
+        aux = pickle.load(alMediciones)
+        zona = int(aux.nro_zona)
+        snap = float(aux.snap)
+
+        medicionesDiarias[zona][0] += snap
+        medicionesDiarias[zona][1] += 1
+
+    zonaSnapMas = -1
+    zonaSnapMenos = 999999
+
+    zonaMas = 0
+    zonaMenos = 0
+
+    for i in range(15):
+        print("Zona "+str(i))
+        snapZona = medicionesDiarias[i][0]/medicionesDiarias[i][1]
+        print(calidadAire(snapZona))
+
+        if(zonaSnapMas < snapZona):
+            zonaSnapMas = snapZona
+            zonaMas = i
         
+        if(zonaSnapMenos > snapZona):
+            zonaSnapMenos = snapZona
+            zonaMenos = i        
+
+    print("Zona con mayor polución: "+str(zonaMas))
+    print("Zona con menor polución: "+str(zonaMenos))
+
 afMediciones = "./mediciones.dat"
 alMediciones = open (afMediciones, "w+b")
 
@@ -37,6 +87,7 @@ else:
     alMaestro = open(afMaestro, "r+b")
 
 inicializarMediciones()
+informeDiario()
 
 alMediciones.close()
 alMaestro.close()
